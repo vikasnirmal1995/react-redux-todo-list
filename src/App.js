75cef4c965fd3bf98b552f40026da1c9f719732c
vikasnirmal1995 <vikasnirmal1995@gmail.com>
2019-08-10 08:45:37 +0530

    redux implement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  addItemToState = (e) =>{
+    e.preventDefault();
+    this.props.addtodo({
+      text: this.refs.addtodo.value
+    })
+    this.refs.addtodo.value = '';
+  }
+  render(){
+    const { todos } = this.props;
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="jumbotron">
+                <h1>This is Home Page</h1>
+                <form onSubmit={this.addItemToState}>
+                  <input type="text" ref="addtodo" /> 
+                  <button type="submit">add todo</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-4">
+              <ul className="list-group">
+                {todos.map((todo, index) =>{
+                  return(
+                    <li className="list-group-item" key={index}>
+                      {todo.text}
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) =>{
+  return{
+    todos: state
+  }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+  return{
+    addtodo: ({text}) =>{
+      dispatch({
+        type: "ADD_TODO",
+        payload: {
+          text: text,
+          complete: false
+        }
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
